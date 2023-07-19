6e5638a7b623e97c9aedb63c48d71bccbd9f31fa
MBonalumi <10607992@polimi.it>
2023-07-19 16:56:38 +0200

    Run.py makes possible to run experiments on terminal doesn't need jupyter notebook

--- src/Run.py
+++ src/Run.py
@@ -1,11 +1,10 @@
 import argparse
-
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 from main import parse_config, instantiate_agents, instantiate_auction, simulation_run
 from tqdm import tqdm
 import time
-
 
 # CONSTANTS
 # INDEXES of the return
@@ -59,20 +58,20 @@ def run_repeated_auctions(num_run, num_runs, results=None, debug=False):
             agent.bidder.num_iterations = num_iter
             if num_run == 0: 
                 if not agent.bidder.isContinuous:
-                    print('\t', agent.name, ': ', agent.bidder.BIDS)
+                    if args.print: print('\t', agent.name, ': ', agent.bidder.BIDS)
                 else:
-                    print('\t', agent.name, ': ', agent.bidder.textContinuous)
+                    if args.print: print('\t', agent.name, ': ', agent.bidder.textContinuous)
 
     if debug:
         for agent in auction.agents:
-            print(agent.name, ': ', agent.bidder.auction_type, end=' | ')
+            if args.print: print(agent.name, ': ', agent.bidder.auction_type, end=' | ')
 
     # Run repeated auctions
     # This logic is encoded in the `simulation_run()` method in main.py
-    # print(num_run, ') ', end='')
+    # if args.print: print(num_run, ') ', end='')
     for i in tqdm(range(num_iter), desc=f'{num_run+1}/{num_runs}', leave=True):
         if debug:
-            print(f'Iteration {i+1} of {num_iter}')
+            if args.print: print(f'Iteration {i+1} of {num_iter}')
 
         # Simulate impression opportunities
         for _ in range(rounds_per_iter):
@@ -107,15 +106,15 @@ def run_repeated_auctions(num_run, num_runs, results=None, debug=False):
 
         last_surplus = [surplus[-1] for surplus in agents_overall_surplus]
         if debug:
-            print(f"\teach agent's surplus: {last_surplus}")
-            print(f"\tsums to {np.array(last_surplus).sum()}")
+            if args.print: print(f"\teach agent's surplus: {last_surplus}")
+            if args.print: print(f"\tsums to {np.array(last_surplus).sum()}")
         
         # Update agents
         # Clear running metrics
         for agent_id, agent in enumerate(auction.agents):
             if(len(agent.logs)>0):
                 if debug:
-                    print(f'\t agent update: {my_agents_names[agent_id]}')
+                    if args.print: print(f'\t agent update: {my_agents_names[agent_id]}')
                 agent.update(iteration=i)
                 # if i==num_iter-1:
                 #     agents_last_avg_utilities[agent_id].append(agent.bidder.expected_utilities)
@@ -152,8 +151,7 @@ def run_repeated_auctions(num_run, num_runs, results=None, debug=False):
             agents_overall_surplus, agents_instant_surplus,\
             agents_regret_history, agents_actionsrewards_history
 
-def construct_graph(data, graph, xlabel, ylabel):
-    fontsize = 16
+def construct_graph(data, graph, xlabel, ylabel, insert_labels=False, fontsize=16):
     # data = np.array([x[index] for x in num_participants_2_metrics]).squeeze().transpose(1,0,2)
 
     y_err = []
@@ -175,14 +173,16 @@ def construct_graph(data, graph, xlabel, ylabel):
     graph.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
     graph.axhline(0, color='black', lw=1, alpha=.7)
 
-    handles, labels = graph.get_legend_handles_labels()
-    legend = graph.legend(reversed(handles),
-                            reversed(labels),
-                            loc='upper left',
-                            bbox_to_anchor=(1.0, 1.0),
-                            fontsize=fontsize)
+    if insert_labels:
+        handles, labels = graph.get_legend_handles_labels()
+        legend = graph.legend(reversed(handles),
+                                reversed(labels),
+                                loc='upper left',
+                                bbox_to_anchor=(1.0, 1.0),
+                                fontsize=fontsize)
     
-def show_graph(runs_results, filename="noname"):
+
+def show_graph(runs_results, filename="noname", printFlag=False):
     plt.ioff()
 
     # # Create a new figure, plot into it, then close it so it never gets displayed
@@ -200,13 +200,16 @@ def show_graph(runs_results, filename="noname"):
     # plt.show()
 
     fontsize = 16
-    subplot_num = 4
+    fig = plt.gcf()
+    fig.set_size_inches(32,18)
+    fig.sharey = 'all'
+    gs = fig.add_gridspec(3, 2)
 
-    fig, axes = plt.subplots(subplot_num,1, sharey='row', figsize=(30,10*subplot_num))
 
-    graph_cumulative_surpluses = axes[0]
-    graph_instant_surpluses = axes[1]
-    graph_regrets_hindsight = axes[2]
+    graph_cumulative_surpluses = fig.add_subplot(gs[0:1, 0:2])
+    graph_cumulative_regrets = fig.add_subplot(gs[1:2, 0:2])
+    graph_instant_surpluses = fig.add_subplot(gs[2:3, 0:1])
+    graph_regrets_hindsight = fig.add_subplot(gs[2:3, 1:2])
 
     graph_cumulative_surpluses.set_title(graph_title, fontsize=fontsize+4)
 
@@ -216,64 +219,64 @@ def show_graph(runs_results, filename="noname"):
     cumulative_surpluses = np.array([x[idx_cumulative_surpluses] for x in runs_results]).squeeze().transpose(1,0,2)
     instant_surpluses = np.array([x[idx_instant_surpluses] for x in runs_results]).squeeze().transpose(1,0,2)
     instant_regrets = np.array([x[idx_regrets] for x in runs_results]).squeeze().transpose(1,0,2)
-    construct_graph(cumulative_surpluses, graph_cumulative_surpluses, '', 'Cumulative Surplus')
-    construct_graph(instant_surpluses, graph_instant_surpluses, '', 'Instant Surplus')
-    construct_graph(instant_regrets, graph_regrets_hindsight, '', 'Instant Regret in Hindsight')
+    construct_graph(cumulative_surpluses, graph_cumulative_surpluses, '', 'Cumulative Surplus', insert_labels=True, fontsize=fontsize)
+    construct_graph(instant_surpluses, graph_instant_surpluses, '', 'Instant Surplus', insert_labels=False, fontsize=fontsize)
+    construct_graph(instant_regrets, graph_regrets_hindsight, '', 'Instant Regret in Hindsight', insert_labels=True, fontsize=fontsize)
 
     #cumulative regrets
-    graph_regrets_cumul = axes[3]
     regrets_cumul = np.zeros_like(instant_regrets)
     for i in range(instant_regrets.shape[0]):
         for j in range(instant_regrets.shape[1]):
             regrets_cumul[i][j] = np.array([instant_regrets[i][j][:h+1].sum() for h in range(instant_regrets.shape[2])])
-    construct_graph(regrets_cumul, graph_regrets_cumul, '', 'Cumulative Regret in Hindsight')
+    construct_graph(regrets_cumul, graph_cumulative_regrets, '', 'Cumulative Regret in Hindsight', insert_labels=True, fontsize=fontsize)
 
 
     fig.tight_layout()
 
     # plt.show()
     ts = time.strftime("%Y%m%d-%H%M", time.localtime())
-    plot_filename = "plots/" + filename + "_" + ts + ".png"
-    plt.savefig(plot_filename)
-    print(f"Plot saved to {plot_filename}")
+    plt.savefig(filename)
+    if args.print: print(f"Plot saved to {filename}")
     plt.close(fig)
 
 
 ############ MAIN ############
 if __name__ == '__main__':
-    print("### 1. importings all done ###")
     # Parse commandline arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('config', type=str, help='Path to config file')
+    parser.add_argument('-nprox', type=int, default=1, help='Number of processors to use')
+    parser.add_argument('-print', type=bool, default=False, help='Whether to print results')
     args = parser.parse_args()
-    print()
-    print()
+    if args.print: print("### 1. importings all done ###")
+    if args.print: print()
+    if args.print: print()
     
     #
     # 2. config file
     #
-    print("### 2. selecting config file ###")
-    config_file = "../config-mine/" + args.config
+    if args.print: print("### 2. selecting config file ###")
+    config_file = "./config-mine/" + args.config + ".json"
     graph_title = config_file
-    print(f'\tUsing config file: {args.config}')
-    print()
-    print()
+    if args.print: print(f'\tUsing config file: {args.config}')
+    if args.print: print()
+    if args.print: print()
 
     # 
     # 3. Parsing config file
     # 
-    print("### 3. parsing config file ###")
+    if args.print: print("### 3. parsing config file ###")
     rng, config, agent_configs, agents2items, agents2item_values,\
     num_runs, max_slots, embedding_size, embedding_var,\
     obs_embedding_size = parse_config(config_file)
 
     num_iter = config['num_iter']
 
-    print('--- Auction ---')
-    print(config['allocation'])
-    print()
+    if args.print: print('--- Auction ---')
+    if args.print: print(config['allocation'])
+    if args.print: print()
 
-    print('--- My Agents ---')
+    if args.print: print('--- My Agents ---')
     my_agents_names = []
     i=0
     for agent in config['agents']:
@@ -281,21 +284,21 @@ if __name__ == '__main__':
             i+=1
             # my_agents_names.append(f'{i}.{agent["bidder"]["type"]} ({agent["name"]})')
             my_agents_names.append(f'{i}. {agent["name"]}')
-            # print(f'{i}) {agent["bidder"]["type"]}')
-    print(my_agents_names)
+            # if args.print: print(f'{i}) {agent["bidder"]["type"]}')
+    if args.print: print(my_agents_names)
 
-    print()
-    print('--- Runs Number ---')
-    print(f"making {config['num_runs']} runs\n  for each, {config['num_iter']} iterations\n    for each, {config['rounds_per_iter']} episodes")
-    print(f"\t -> total: {config['num_runs']*config['num_iter']*config['rounds_per_iter']}")
+    if args.print: print()
+    if args.print: print('--- Runs Number ---')
+    if args.print: print(f"making {config['num_runs']} runs\n  for each, {config['num_iter']} iterations\n    for each, {config['rounds_per_iter']} episodes")
+    if args.print: print(f"\t -> total: {config['num_runs']*config['num_iter']*config['rounds_per_iter']}")
 
-    print()
-    print()
+    if args.print: print()
+    if args.print: print()
 
     #
     # 4. overwriting products
     #
-    print("### 4. overwriting products ###")
+    if args.print: print("### 4. overwriting products ###")
     agents_names = list(agents2items.keys())
     assert agents_names[0] == list(agents2item_values.keys())[0] 
     obj_embed = agents2items[ agents_names[0] ] [0]
@@ -307,14 +310,14 @@ if __name__ == '__main__':
 
     # obj_embed, obj_value
     for agent_name in agents_names:
-        print(agents2items[agent_name], " -> ", agents2item_values[agent_name])
-    print()
-    print()
+        if args.print: print(agents2items[agent_name], " -> ", agents2item_values[agent_name])
+    if args.print: print()
+    if args.print: print()
 
     #
     # 5. running experiment
     #
-    print("### 5. running experiment ###")
+    if args.print: print("### 5. running experiment ###")
 
     from threading import Thread
     secondary_outputs = []
@@ -324,12 +327,12 @@ if __name__ == '__main__':
 
     threads = [Thread(target=run_repeated_auctions, args=(i, num_runs, runs_results, debug)) for i in range(num_runs)]
 
-    n_prox = 12 #num_run
+    n_prox = args.nprox
 
     i=0
     j=0
     while i < num_runs:
-        # print(i,' &&& ',j)
+        # if args.print: print(i,' &&& ',j)
         for j in range(n_prox):
             if i+j >= len(threads):
                 break
@@ -342,18 +345,18 @@ if __name__ == '__main__':
         
         i+=n_prox
 
-    print("RUN IS DONE")
-    print()
-    print()
+    if args.print: print("RUN IS DONE")
+    if args.print: print()
+    if args.print: print()
 
     #
     # 6. print surpluses
     #
-    print("### 6. saving results ###")
-    print(my_agents_names)
+    if args.print: print("### 6. saving results ###")
+    if args.print: print(my_agents_names)
     total_surpluses = [[] for _ in range(len(my_agents_names))]
 
-    np.set_printoptions(precision=2, floatmode='fixed', sign=' ')
+    # np.set_printoptions(precision=2, floatmode='fixed', sign=' ')
 
     for h, run in enumerate(runs_results):
         # r, w, s, a_s, i_s = run
@@ -366,11 +369,57 @@ if __name__ == '__main__':
 
         print_surpluses = ' '.join('{:7.2f}'.format(x) for x in surpluses)
         print_cumulatives = ' '.join('{:7.2f}'.format(x) for x in cumulatives)
-        print(f'Run {h+1:=2}/{num_runs} -> surpluses: {print_surpluses}     |     last cumulative {print_cumulatives}')
+        if args.print: print(f'Run {h+1:=2}/{num_runs} -> surpluses: {print_surpluses}     |     last cumulative {print_cumulatives}')
 
     # overall
     total_surpluses = np.array( [np.array(x).mean() for x in total_surpluses] )
     print_overall = ' '.join('{:7.2f}'.format(np.array(x).mean()) for x in total_surpluses)
-    print('\n     PER-RUN AVERAGE: ', '[' + (print_overall) + ']')
-    print()
-    print()
+    if args.print: print('\n     PER-RUN AVERAGE: ', '[' + (print_overall) + ']')
+    if args.print: print()
+    if args.print: print()
+
+
+    #
+    # 7. save results
+    # 
+    if args.print: print("### 7. saving results ###")
+
+    # save results
+    ts = time.strftime("%Y%m%d-%H%M", time.localtime())
+
+    #create folder
+    file_prefix = args.config+"_"+ts
+    folder_name = "./src/results/"+args.config+"_"+ts
+    os.makedirs(folder_name, exist_ok=True)
+
+    results_filename = folder_name + "/" + file_prefix + "_results.txt"
+    with open(results_filename, 'w') as f:
+        f.write(f'config: {args.config}\n')
+        f.write(f'num_runs: {num_runs}\n')
+        f.write(f'num_iter: {num_iter}\n')
+        f.write(f'rounds_per_iter: {config["rounds_per_iter"]}\n')
+        f.write(f'num_agents: {len(my_agents_names)}\n')
+        f.write(f'agents: {my_agents_names}\n')
+        f.write(f'agents2items: {agents2items}\n')
+        f.write(f'agents2item_values: {agents2item_values}\n')
+        f.write(f'embedding_size: {embedding_size}\n')
+        f.write(f'embedding_var: {embedding_var}\n')
+        f.write(f'obs_embedding_size: {obs_embedding_size}\n')
+
+        f.write(f'\n\n\n')
+
+    if args.print: print("results saved in ", results_filename)
+
+    data_filename = folder_name + "/" + file_prefix + "_data.npy"
+    np.save(data_filename, runs_results)
+
+    if args.print: print("data saved in ", data_filename)
+
+
+    #
+    # 8. save plot
+    #
+    if args.print: print("### 8. saving plot ###")
+
+    plot_filename = folder_name + "/" + file_prefix + "_plot.png"
+    show_graph(runs_results, plot_filename, args.print)
